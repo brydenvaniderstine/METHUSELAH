17b2f2344af421f512546e769f0e4d6c088f5942
brydenmcleodvan <brydenvaniderstine@gmail.com>
2026-05-06 11:49:17 -0700

    v1.0.6 // shell height 100% // log stretch fix

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ body::before {
 }
 
 .shell {
-  display: flex; flex-direction: column;
+  display: flex; flex-direction: column; height: 100%;
   height: 100%; width: 100%; max-width: 100%;
   margin: 0 auto; padding: 12px; position: relative; z-index: 1; gap: 16px;
 }
